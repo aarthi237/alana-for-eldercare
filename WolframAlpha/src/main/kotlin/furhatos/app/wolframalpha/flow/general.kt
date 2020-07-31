8a234877102971b24305be24a21639bda2b7bbac
Arthidevi Balavignesh <ab237@hw.ac.uk>
2020-07-31 02:26:56 +0100

    furhat integration

--- WolframAlpha/src/main/kotlin/furhatos/app/wolframalpha/flow/general.kt
+++ WolframAlpha/src/main/kotlin/furhatos/app/wolframalpha/flow/general.kt
@@ -8,6 +8,7 @@ val defaultVoice = PollyVoice("Matthew")
 
 val idle: State = state {
     init {
+        println("idleinit")
         furhat.setTexture("male")
         furhat.voice = defaultVoice
         if (users.count > 0) {
@@ -17,10 +18,12 @@ val idle: State = state {
     }
 
     onEntry {
+        println("idleentry")
         furhat.attendNobody()
     }
 
     onUserEnter {
+        println("idleonenter")
         furhat.attend(it)
         goto(start)
     }
@@ -28,6 +31,7 @@ val idle: State = state {
 
 val interaction: State = state {
     onUserLeave(instant = true) {
+        println("int-onUserLeave")
         if (users.count > 0) {
             if (it == users.current) {
                 furhat.attend(users.other)
@@ -36,11 +40,13 @@ val interaction: State = state {
                 furhat.glance(it)
             }
         } else {
+            println("int-goidle")
             goto(idle)
         }
     }
 
     onUserEnter(instant = true) {
+        println("int-onUserEnter")
         furhat.glance(it)
     }
 
--- WolframAlpha/src/main/kotlin/furhatos/app/wolframalpha/flow/wolframalpha.kt
+++ WolframAlpha/src/main/kotlin/furhatos/app/wolframalpha/flow/wolframalpha.kt
@@ -3,6 +3,9 @@ package furhatos.app.wolframalpha.flow
 import furhatos.nlu.common.*
 import furhatos.flow.kotlin.*
 import furhatos.gestures.Gestures
+import org.json.JSONObject
+import khttp.post
+import org.json.JSONArray
 
 val BASE_URL = "http://localhost:5005/webhooks/rest/webhook" // Endpoint for Wolfram Alpha's API with answers tailored for spoken interactions
 //val APP_ID = "EQKLKA-5EGTXH74UG" // Test account, feel free to use it for testing.
@@ -12,39 +15,79 @@ val TIMEOUT = 4000 // 4 seconds
 // Start state containing everything except the query to the API
 val start : State = state(interaction) {
 
-    //onEntry {
-    //    furhat.ask("Hi there!")
-    //}
-
+    onEntry {
+        furhat.ask("Hi there!")
+    }
     onResponse {
-        // Query done in query state below, with its result saved here since we're doing a call
         val response = call(query(it.text)) as String
         furhat.say(response)
+        goto(start1)
+    }
+
+}
+
+val start1 : State = state(interaction) {
+
+    onEntry {
+        furhat.ask(" ")
+    }
+    onResponse {
+        val response = call(query(it.text)) as String
+        furhat.say(response)
+        if(it.text != "Bye" && response != "OK Have a Nice day, Bye Bye") {
+            goto(start2)
+        }
     }
 }
+
+val start2 : State = state(interaction) {
+
+    onEntry {
+        furhat.ask(" ")
+    }
+    onResponse {
+        val response = call(query(it.text)) as String
+        furhat.say(response)
+        if(it.text != "Bye" && response != "OK Have a Nice day, Bye Bye") {
+            goto(start1)
+        }
+    }
+}
+
 
 // State to conduct the query to the API
 fun query(question: String) = state {
     onEntry {
-        //val query = "$BASE_URL?i=$question&appid=$APP_ID"
+        val query = "$BASE_URL"
 
         /* Call to WolframAlpha API made in an anynomous substate (https://docs.furhat.io/flow/#calling-anonymous-states)
          to allow our timeout below to stop the call if it takes to long. Note that you explicitly has to cast the result to a String.
           */
+        val rootObject= JSONObject()
+        rootObject.put("sender","test123")
+        rootObject.put("message",question)
         val response = call {
             //khttp.get(query).text
-            khttp.post(
-                url = $BASE_URL,
-                data = mapOf("sender" to "test123", "message" to $question))
+            post(url = BASE_URL, data = "{\"sender\":\"test123\", \"message\": \"+$question+\"}").text
         } as String
 
+        println(response)
+
+        val obj = JSONArray(response)
+        //println(obj)
+        var res = ""
+        for (i in 0 until obj.length()) {
+            val item = obj.getJSONObject(i)
+            res += "  " + item["text"]
+            println(item["text"])
+        }
         // Reply to user depending on the returned response
         val reply = when {
-            FAILED_RESPONSES.contains(response) -> {
+            FAILED_RESPONSES.contains("test") -> {
                 println("No answer from rasa")
                 "Sorry bro, can't understand"
             }
-            else -> response
+            else -> res
         }
         // Return the response
         terminate(reply)
