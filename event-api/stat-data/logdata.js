0e6c0857fee8e38e708c5b5be04c636c43273099
Arthidevi Balavignesh <ab237@hw.ac.uk>
2020-08-01 12:15:28 +0100

    log file location updated

--- event-api/stat-data/logdata.js
+++ event-api/stat-data/logdata.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const logdb = new sqlite3.Database('../chat_log.db');
+const logdb = new sqlite3.Database('./chat_log.db');
 
 exports.getLogDataFromDb = async (senderId) => {
     let sql = `select * from events where sender_id LIKE "${senderId}" order by timestamp`;
